eea8ac5f168c8511e46efe49693b7d191c39b668
Daytron <ryangilera@gmail.com>
2015-05-26 08:18:16 +0100

    Update schema and add script for data inserts

---
+++ schema/UserInsert.sql
@@ -0,0 +1,212 @@
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Ryan', 'Gilera');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Rand', 'Althor');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Egwene', 'Alvere');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Matrim', 'Cauthon');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Nynaeve', 'Almeara');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Perrin', 'Aybara');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Moiraine', 'Damodred');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Lews Therin', 'Telamon');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Elayne', 'Trakand');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Verin', 'Mathwin');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Cadsuane', 'Melaidhrin');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Lan', 'Mandragoran');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Faile', 'Bashere');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Min', 'Farshaw');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Thom', 'Merrilin');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Padan', 'Fain');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Mazrim', 'Taim');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Artur', 'Hawkwing');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Birgitte', 'Silverbow');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Artur', 'Hawkwing');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Eamon', 'Valda');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Elyas', 'Machera');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Gaidal', 'Cain');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Gareth', 'Bryne');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Ilyena', 'Moerelle');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Jain', 'Farstrider');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Juilin', 'Sandar');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Masema', 'Dagar');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Mattin', 'Stepaneos');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Morgase', 'Trakand');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Nalesean', 'Aldiaya');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Nicola', 'Treehill');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Setalle', 'Anan');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Tigraine', 'Mantear');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Katerine', 'Alruddin');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Teslyn', 'Baradon');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Narenwin', 'Barda');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Davram', 'Bashere');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Adelorna', 'Adelorna');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Myrelle', 'Berengari');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Joiya', 'Byir');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Bayle', 'Domon');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Asim', 'Majeed');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Baba', 'Sheba');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Aster', 'Mekonnen');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Amarjeet', 'Singh');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Anwar', 'Ul Hawq');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Salvatore', 'Maiorano');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Tendai', 'Mhlanga');
+INSERT INTO `appschema`.`User` (`first_name`, `last_name`) VALUES ('Usman', 'Butt');
+
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126250,'youview',1);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126251,'youview',2);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126252,'youview',3);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126253,'youview',4);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126254,'youview',5);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126255,'youview',6);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126256,'youview',7);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126257,'youview',8);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126258,'youview',9);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126259,'youview',10);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126260,'youview',11);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126261,'youview',12);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126262,'youview',13);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126263,'youview',14);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126264,'youview',15);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126265,'youview',16);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126266,'youview',17);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126267,'youview',18);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126268,'youview',19);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126269,'youview',20);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126270,'youview',21);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126271,'youview',22);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126272,'youview',23);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126273,'youview',24);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126274,'youview',25);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126275,'youview',26);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126276,'youview',27);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126277,'youview',28);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126278,'youview',29);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126279,'youview',30);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126280,'youview',31);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126281,'youview',32);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126282,'youview',33);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126283,'youview',34);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126284,'youview',35);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126285,'youview',36);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126286,'youview',37);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126287,'youview',38);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126288,'youview',39);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126289,'youview',40);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126290,'youview',41);
+INSERT INTO `appschema`.`Student` (`id`,`password`,`person_id`)
+VALUES (126291,'youview',42);
+
+
+INSERT INTO `appschema`.`Lecturer` (`email`,`password`,`person_id`)
+VALUES ('asim.majeed@gsm.org.uk','youview',43);
+INSERT INTO `appschema`.`Lecturer` (`email`,`password`,`person_id`)
+VALUES ('baba.sheba@gsm.org.uk','youview',44);
+INSERT INTO `appschema`.`Lecturer` (`email`,`password`,`person_id`)
+VALUES ('aster.mekonnen@gsm.org.uk','youview',45);
+INSERT INTO `appschema`.`Lecturer` (`email`,`password`,`person_id`)
+VALUES ('amarjeet.singh@gsm.org.uk','youview',46);
+INSERT INTO `appschema`.`Lecturer` (`email`,`password`,`person_id`)
+VALUES ('anwar.ulhawq@gsm.org.uk','youview',47);
+INSERT INTO `appschema`.`Lecturer` (`email`,`password`,`person_id`)
+VALUES ('salvatore.maiorano@gsm.org.uk','youvie',48);
+INSERT INTO `appschema`.`Lecturer` (`email`,`password`,`person_id`)
+VALUES ('tendai.mhlanga@gsm.org.uk','youview',49);
+INSERT INTO `appschema`.`Lecturer` (`email`,`password`,`person_id`)
+VALUES ('usman.butt@gsm.org.uk','youview',50);
+
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Computer Science');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Computer Science with Business Informatics');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Human Resources Management with Information Systems');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Business Management and Information Technologyth Information Systems');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Business Management with Human Resources Management');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Accounting and Finance');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('MBA Strategic Management Pathway');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('MBA Marketing Pathway');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Business Management');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Business Management (Travel and Tourism Pathway)');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('MSc Oil and Gas Management');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('BSc Human Resources Management Pathway');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('MBA Financial Management Pathway');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('Executive MBA');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('Executive MBA Health Services Management');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('Executive MSc Leading and Managing in Healthcare Organisations');
+INSERT INTO `appschema`.`Course` (`name`) VALUES ('Diploma in Procurement and Supply');
+
+INSERT INTO `appschema`.`Module` VALUES ('PC107_332_C15','Programming 1');
+INSERT INTO `appschema`.`Module` VALUES ('PC108_333_C15','Information Technology & Professional Skills');
+INSERT INTO `appschema`.`Module` VALUES ('PC109_334_C15','Computer Systems');
+INSERT INTO `appschema`.`Module` VALUES ('SC101N_572_C15','Support Class BSc Level 4');
+INSERT INTO `appschema`.`Module` VALUES ('PC110_335_C15','Programming 2');
+INSERT INTO `appschema`.`Module` VALUES ('PC111_336_C15','Quantitative Methods for Computing');
+INSERT INTO `appschema`.`Module` VALUES ('PC112_337_C15','Software Engineering and Human Computer Interaction');
+INSERT INTO `appschema`.`Module` VALUES ('PC206_339_C15','Database Management Systems');
+INSERT INTO `appschema`.`Module` VALUES ('PC209_340_C15','Systems Analysis & Design');
+INSERT INTO `appschema`.`Module` VALUES ('PC210_341_C15','Artificial Intelligence');
+INSERT INTO `appschema`.`Module` VALUES ('PC205_338_C15','Computer Graphics');
+INSERT INTO `appschema`.`Module` VALUES ('PI207_312_C15','Internet Development: Coding and Programming');
+INSERT INTO `appschema`.`Module` VALUES ('PI208_313_C15','Telecommunications and Networking');
+INSERT INTO `appschema`.`Module` VALUES ('PC302_343_C15','Mobile Devices and Applications');
+INSERT INTO `appschema`.`Module` VALUES ('PC303_344_C15','E-Commerce');
+INSERT INTO `appschema`.`Module` VALUES ('PC304_345_C15','Software Development');
+INSERT INTO `appschema`.`Module` VALUES ('_C15','');
+INSERT INTO `appschema`.`Module` VALUES ('_C15','');
+INSERT INTO `appschema`.`Module` VALUES ('PM302A_583_C15','Financial Research Project');
+INSERT INTO `appschema`.`Module` VALUES ('_C15','');
+INSERT INTO `appschema`.`Module` VALUES ('_C15','');
+INSERT INTO `appschema`.`Module` VALUES ('_C15','');
+INSERT INTO `appschema`.`Module` VALUES ('_C15','');
+INSERT INTO `appschema`.`Module` VALUES ('_C15','');
+INSERT INTO `appschema`.`Module` VALUES ('_C15','');
+
+INSERT INTO `appschema`.`CourseModule` (`course_id`,`module_id`) 
+VALUES (1,'PC107_332_C15');
+INSERT INTO `appschema`.`CourseModule` (`course_id`,`module_id`) 
+VALUES (2,'PC107_332_C15');
+INSERT INTO `appschema`.`CourseModule` (`course_id`,`module_id`) 
+VALUES (,'');
+INSERT INTO `appschema`.`CourseModule` (`course_id`,`module_id`) 
+VALUES (,'');
+INSERT INTO `appschema`.`CourseModule` (`course_id`,`module_id`) 
+VALUES (,'');
+INSERT INTO `appschema`.`CourseModule` (`course_id`,`module_id`) 
+VALUES (,'');
+INSERT INTO `appschema`.`CourseModule` (`course_id`,`module_id`) 
+VALUES (6,'PM302A_583_C15');

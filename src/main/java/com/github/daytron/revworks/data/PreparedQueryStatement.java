17892f13f7f0127e38d21590d22b4cc33e4f3b97
Daytron <ryangilera@gmail.com>
2015-06-07 22:23:02 +0100

    Save the newly created script into the enum

--- src/main/java/com/github/daytron/revworks/data/PreparedQueryStatement.java
+++ src/main/java/com/github/daytron/revworks/data/PreparedQueryStatement.java
@@ -146,7 +146,20 @@ public enum PreparedQueryStatement {
             + "INNER JOIN Lecturer ON Lecturer.user_id = Class.lecturer_user_id "
             + "INNER JOIN User ON User.id = Lecturer.user_id "
             + "WHERE Class.semester_id = ? AND "
-            + "Coursework.student_user_id = ?;");
+            + "Coursework.student_user_id = ?;"),
+    LECTURER_SELECT_COURSEWORK("SELECT Coursework.id AS id, "
+            + "Coursework.title AS title, "
+            + "Coursework.date_submitted AS dateSubmitted, "
+            + "Coursework.file AS file, "
+            + "Coursework.file_extension As fileExtension, "
+            + "Coursework.student_user_id AS studentUserID, "
+            + "Student.student_id AS studentID, "
+            + "User.first_name AS studentFirstName, "
+            + "User.last_name AS studentLastName "
+            + "FROM Coursework "
+            + "INNER JOIN Student ON Student.user_id = Coursework.student_user_id "
+            + "INNER JOIN User ON User.id = Coursework.student_user_id "
+            + "WHERE Coursework.class_id = ?;");
 
     private final String query;
 

d0f6408ddfbebc4229a4575463106203c925f535
Daytron <ryangilera@gmail.com>
2015-06-08 19:01:04 +0100

    Finish Hotfix-LecturerModuleView
    Conflicts:
    	.gitignore (reverted from commit fde0a18809fa8e5be738bf332cf8cd3e5b0bcc2a)

--- schema/LecturerSelectClass.sql
+++
@@ -1,8 +0,0 @@
-SELECT Class.id AS id, 
-Class.module_id AS moduleId, 
-Module.name AS moduldeName 
-FROM Class 
-INNER JOIN Module ON Module.id = Class.module_id 
-INNER JOIN Semester ON Semester.id = Class.semester_id 
-WHERE Class.lecturer_user_id = 47 AND 
-Class.semester_id = 'c15';
--- schema/StudentInsertCoursework.sql
+++
@@ -1,2 +0,0 @@
-insert into Coursework(title,date_submitted,file,file_extension,student_user_id,class_id)
-values (?,now(),?,?,?,?);

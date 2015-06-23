81a6a871f351e95ad5a2f280bed35fd5f9c336a8
Daytron <ryangilera@gmail.com>
2015-06-23 17:34:30 +0100

    Delete derived duplicate

--- schema/SelectCommentDesc.sql
+++
@@ -1,7 +0,0 @@
-SELECT Comment.id AS id, 
-Comment.message AS message, 
-Comment.date_submitted AS dateSubmitted, 
-Comment.is_student_to_lecturer AS isStudentToLecturer 
-FROM Comment 
-WHERE Comment.review_id = ? 
-ORDER BY Comment.date_submitted DESC;

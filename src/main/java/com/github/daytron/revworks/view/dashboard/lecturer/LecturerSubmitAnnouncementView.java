60c969be45474501b67585775ed8ea03d8e8241e
Daytron <ryangilera@gmail.com>
2015-07-09 19:11:24 +0100

    Add a message preview in submit announcement view for lecturers

--- src/main/java/com/github/daytron/revworks/view/dashboard/lecturer/LecturerSubmitAnnouncementView.java
+++ src/main/java/com/github/daytron/revworks/view/dashboard/lecturer/LecturerSubmitAnnouncementView.java
@@ -20,14 +20,18 @@ import com.github.daytron.revworks.event.AppEventBus;
 import com.github.daytron.revworks.model.ClassTable;
 import com.github.daytron.revworks.service.CurrentUserSession;
 import com.github.daytron.revworks.util.NotificationUtil;
+import com.vaadin.data.Property;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.RichTextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
@@ -49,6 +53,8 @@ public class LecturerSubmitAnnouncementView extends VerticalLayout implements Vi
 
     private boolean isInitialised = false;
     private CopyOnWriteArrayList<ClassTable> listOfClassTables;
+    
+    private Label previewContent;
 
     public LecturerSubmitAnnouncementView() {
     }
@@ -63,6 +69,8 @@ public class LecturerSubmitAnnouncementView extends VerticalLayout implements Vi
         
         if (!isInitialised) {
             this.listOfClassTables = CurrentUserSession.getCurrentClassTables();
+            
+            this.previewContent = new Label("&nbsp;");
             initView();
             isInitialised = true;
         }
@@ -86,14 +94,65 @@ public class LecturerSubmitAnnouncementView extends VerticalLayout implements Vi
     }
 
     private Component createNewAnnouncementForm() {
+        HorizontalLayout horizontalLayout = 
+                new HorizontalLayout();
+        horizontalLayout.setWidth("100%");
+        
+        FormLayout leftLayout = createEditorComponent();
+        horizontalLayout.addComponent(leftLayout);
+        
+        CssLayout rightLayout = createPreviewSection();
+        horizontalLayout.addComponent(rightLayout);
+        
+        horizontalLayout.setExpandRatio(leftLayout, 1);
+        horizontalLayout.setExpandRatio(rightLayout, 1);
+        
+        return horizontalLayout;
+    }
+    
+    private CssLayout createPreviewSection() {
+        final CssLayout wrapperItem = new CssLayout();
+        wrapperItem.setSizeFull();
+        wrapperItem.setStyleName(ValoTheme.LAYOUT_CARD);
+        
+        // Create Header
+        final HorizontalLayout layoutHeader = new HorizontalLayout();
+        layoutHeader.addStyleName("v-panel-caption");
+        layoutHeader.setWidth("100%");
+
+        Label titleLabel = new Label("Message Preview");
+        titleLabel.setStyleName(ValoTheme.LABEL_BOLD);
+        titleLabel.setSizeFull();
+        layoutHeader.addComponent(titleLabel);
+        
+        wrapperItem.addComponent(layoutHeader);
+             
+        // Preview area
+        Panel previewPanel = new Panel();
+        previewPanel.setWidth("100%");
+        previewPanel.setHeight(25, Unit.EM);
+        
+        previewContent.setContentMode(ContentMode.HTML);
+        previewContent.setWidth("100%");
+        previewContent.setHeightUndefined();
+        previewContent.addStyleName("label-preview");
+        
+        previewPanel.setContent(previewContent);
+        
+        wrapperItem.addComponent(previewPanel);
+        
+        return wrapperItem;
+    }
+    
+    private FormLayout createEditorComponent() {
         FormLayout contentLayout = new FormLayout();
 
         contentLayout.setSizeFull();
         contentLayout.setMargin(true);
         contentLayout.setSpacing(true);
 
-        final ComboBox moduleComboBox = new ComboBox("Select class");
-        moduleComboBox.setWidth(25, UNITS_EM);
+        final ComboBox moduleComboBox = new ComboBox("Class");
+        moduleComboBox.setWidth("100%");
 
         for (ClassTable classItem : this.listOfClassTables) {
             moduleComboBox.addItem(classItem.getModuleName());
@@ -117,20 +176,33 @@ public class LecturerSubmitAnnouncementView extends VerticalLayout implements Vi
 
         // Title area
         final TextField titleTextField = new TextField("Title");
-        titleTextField.setWidth(25, UNITS_EM);
+        titleTextField.setWidth("100%");
         // As define in database constraints
         titleTextField.setMaxLength(45);
         contentLayout.addComponent(titleTextField);
 
         // Message area
         final RichTextArea richTextArea
-                = new RichTextArea("Announcement message");
-        richTextArea.setWidth(35, UNITS_EM);
+                = new RichTextArea("Message");
+        richTextArea.setWidth("100%");
+        richTextArea.setHeight(20, Unit.EM);
+        
+        richTextArea.setImmediate(true);
         contentLayout.addComponent(richTextArea);
+        
+        richTextArea.addValueChangeListener(new Property.ValueChangeListener() {
 
+            @Override
+            public void valueChange(Property.ValueChangeEvent event) {
+                previewContent.setValue(richTextArea.getValue());
+            }
+        });
+
+        // Create buttons
         HorizontalLayout buttonBar = new HorizontalLayout();
         Button submitButton = new Button("Submit");
         Button resetButton = new Button("Reset");
+        Button previewButton = new Button("Preview");
 
         final List<ClassTable> copyOfClassTables = this.listOfClassTables;
         submitButton.setDisableOnClick(true);
@@ -189,14 +261,26 @@ public class LecturerSubmitAnnouncementView extends VerticalLayout implements Vi
                 richTextArea.focus();
             }
         });
+        
+        previewButton.addClickListener(new Button.ClickListener() {
 
-        buttonBar.addComponents(submitButton, resetButton);
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                previewContent.setValue(richTextArea.getValue());
+            }
+        });
+
+        // Add extra space bewteen text area and button bar
+        Label spaceLabel = new Label("&nbsp;",ContentMode.HTML);
+        spaceLabel.setHeight(10, Unit.PIXELS);
+        contentLayout.addComponent(spaceLabel);
+        
+        buttonBar.addComponents(submitButton, resetButton, previewButton);
         buttonBar.setSpacing(true);
 
         contentLayout.addComponent(buttonBar);
 
         return contentLayout;
-
     }
 
 }

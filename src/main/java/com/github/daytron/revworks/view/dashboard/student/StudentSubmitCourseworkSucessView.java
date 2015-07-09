ba04f2c26838082e01c22823d90c54f766f5587d
Daytron <ryangilera@gmail.com>
2015-07-09 23:59:11 +0100

    Refactor submit coursework success view for better visual

--- src/main/java/com/github/daytron/revworks/view/dashboard/student/StudentSubmitCourseworkSucessView.java
+++ src/main/java/com/github/daytron/revworks/view/dashboard/student/StudentSubmitCourseworkSucessView.java
@@ -15,11 +15,13 @@
  */
 package com.github.daytron.revworks.view.dashboard.student;
 
+import com.github.daytron.revworks.data.FontAwesomeIcon;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * A view for when a student has successfully submitted a coursework.
@@ -34,11 +36,65 @@ public class StudentSubmitCourseworkSucessView extends VerticalLayout
 
     public StudentSubmitCourseworkSucessView() {
         setWidth("100%");
+        setSpacing(true);
 
-        Label successLabel = new Label("You've successfully submitted your coursework.");
-        successLabel.setStyleName(ValoTheme.LABEL_SUCCESS);
+        // Extra top space
+        Label gapLabelTop = new Label();
+        gapLabelTop.setHeight(20, Unit.PIXELS);
+        addComponent(gapLabelTop);
+        
+        // Check image Row
+        final HorizontalLayout checkLayout = new HorizontalLayout();
+        checkLayout.setWidth("100%");
+        checkLayout.setSpacing(true);
+        
+        Label leftCheckSpaceLabel = new Label(" ");
+        leftCheckSpaceLabel.setWidth("100%");
+        checkLayout.addComponent(leftCheckSpaceLabel);
+        
+        Label checkLabel = new Label(FontAwesomeIcon.CHECK_CIRCLE
+                .get5xSizeWithNoSpace(),
+                ContentMode.HTML);
+        checkLabel.addStyleName("check-image");
+        checkLabel.setSizeUndefined();
+        checkLayout.addComponent(checkLabel);
+        
+        Label rightCheckSpaceLabel = new Label(" ");
+        rightCheckSpaceLabel.setWidth("100%");
+        checkLayout.addComponent(rightCheckSpaceLabel);
 
-        addComponent(successLabel);
+        checkLayout.setExpandRatio(leftCheckSpaceLabel, 1);
+        checkLayout.setExpandRatio(rightCheckSpaceLabel, 1);
+        
+        addComponent(checkLayout);
+        
+        // Success text row
+        final HorizontalLayout successTextLayout = new HorizontalLayout();
+        successTextLayout.setWidth("100%");
+        successTextLayout.setSpacing(true);
+        
+        Label leftSuccessSpaceLabel = new Label(" ");
+        leftSuccessSpaceLabel.setWidth("100%");
+        successTextLayout.addComponent(leftSuccessSpaceLabel);
+        
+        Label successLabel = new Label("You've successfully submitted your "
+                + "coursework.");
+        successLabel.setSizeUndefined();
+        successTextLayout.addComponent(successLabel);
+        
+        Label rightSuccessSpaceLabel = new Label(" ");
+        rightSuccessSpaceLabel.setWidth("100%");
+        successTextLayout.addComponent(rightSuccessSpaceLabel);
+        
+        successTextLayout.setExpandRatio(leftSuccessSpaceLabel, 1);
+        successTextLayout.setExpandRatio(rightSuccessSpaceLabel, 1);
+        
+        addComponent(successTextLayout);
+        
+        // Extra bottom space
+        Label gapLabelBottom = new Label();
+        gapLabelBottom.setHeight(9, Unit.EM);
+        addComponent(gapLabelBottom);
 
     }
 
